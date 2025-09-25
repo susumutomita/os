d531a0c118ea965b4d0df557b87ba6130be237d9
susumutomita <oyster880@gmail.com>
2025-09-26 08:22:52 +0900

    add diagram2-9

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@
 
 use core::mem::offset_of;
 use core::mem::size_of;
+
 #[cfg(not(test))]
 use core::panic::PanicInfo;
 use core::ptr::null_mut;
-use core::slice;
 
 type EfiVoid = u8;
 type EfiHandle = u64;
@@ -36,14 +36,22 @@ const EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID: EfiGuid = EfiGuid {
 
 #[no_mangle]
 fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
-    let efi_graphics_output_protocol = locate_graphic_protocol(efi_system_table).unwrap();
-    let vram_addr = efi_graphics_output_protocol.mode.frame_buffer_base;
-    let vram_byte_size = efi_graphics_output_protocol.mode.frame_buffer_base;
-    let vram = unsafe {
-        slice::from_raw_parts_mut(vram_addr as *mut u32, vram_byte_size / size_of::<u32>())
-    };
-    for e in vram {
-        *e = 0xffffff;
+    // let efi_graphics_output_protocol = locate_graphic_protocol(efi_system_table).unwrap();
+    // let vram_addr = efi_graphics_output_protocol.mode.frame_buffer_base;
+    // let vram_byte_size = efi_graphics_output_protocol.mode.frame_buffer_base;
+    // let vram = unsafe {
+    //     slice::from_raw_parts_mut(vram_addr as *mut u32, vram_byte_size / size_of::<u32>())
+    // };
+    // for e in vram {
+    //     *e = 0xffffff;
+    // }
+    let mut vram = init_vram(efi_system_table).expect("init_vram failed");
+    for y in 0..vram.height {
+        for x in 0..vram.width {
+            if let Some(pixel) = vram.pixel_at_mut(x, y) {
+                *pixel = 0x00ff00;
+            }
+        }
     }
 
     // println!("Hello, world!");
@@ -128,9 +136,82 @@ enum EfiStatus {
 #[panic_handler]
 #[allow(clippy::empty_loop)]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    loop {
+        hlt()
+    }
 }
 
+#[inline]
+fn hlt() {
+    unsafe {
+        core::arch::asm!("hlt", options(nomem, nostack, preserves_flags));
+    }
+}
+
+trait Bitmap {
+    fn bytes_per_pixel(&self) -> i64;
+    fn pixels_per_line(&self) -> i64;
+    fn width(&self) -> i64;
+    fn height(&self) -> i64;
+    fn buf_mut(&mut self) -> &mut [u8];
+    unsafe fn unchecked_pixel_at_mut(&mut self, x: i64, y: i64) -> &mut u32 {
+        let offset = ((y * self.pixels_per_line() + x) * self.bytes_per_pixel()) as usize;
+        let ptr = (self.buf_mut().as_mut_ptr()).add(offset) as *mut u32;
+        &mut *ptr
+    }
+    fn pixel_at_mut(&mut self, x: i64, y: i64) -> Option<&mut u32> {
+        if self.is_in_x_range(x) && self.is_in_y_range(y) {
+            unsafe { Some(&mut *(self.unchecked_pixel_at_mut(x, y))) }
+        } else {
+            Some(unsafe { self.unchecked_pixel_at_mut(x, y) })
+        }
+    }
+    fn is_in_x_range(&self, x: i64) -> bool {
+        x >= 0 && x < self.width()
+    }
+    fn is_in_y_range(&self, y: i64) -> bool {
+        y >= 0 && y < self.height()
+    }
+}
+
+#[derive(Clone, Copy)]
+struct VramBufferInfo {
+    buf: *mut u8,
+    width: i64,
+    height: i64,
+    pixels_per_line: i64,
+}
+
+impl Bitmap for VramBufferInfo {
+    fn bytes_per_pixel(&self) -> i64 {
+        4
+    }
+    fn pixels_per_line(&self) -> i64 {
+        self.pixels_per_line
+    }
+    fn width(&self) -> i64 {
+        self.width
+    }
+    fn height(&self) -> i64 {
+        self.height
+    }
+    fn buf_mut(&mut self) -> &mut [u8] {
+        unsafe {
+            let size = (self.height * self.pixels_per_line * self.bytes_per_pixel()) as usize;
+            core::slice::from_raw_parts_mut(self.buf, size)
+        }
+    }
+}
+
+fn init_vram(efi_system_table: &EfiSystemTable) -> Result<VramBufferInfo> {
+    let gp = locate_graphic_protocol(efi_system_table)?;
+    Ok(VramBufferInfo {
+        buf: gp.mode.frame_buffer_base as *mut u8,
+        width: gp.mode.info.horizontal_resolution as i64,
+        height: gp.mode.info.vertical_resolution as i64,
+        pixels_per_line: gp.mode.info.pixels_per_scan_line as i64,
+    })
+}
 // テストモジュールを追加
 #[cfg(test)]
 mod tests {
